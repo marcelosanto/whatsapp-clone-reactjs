53b21d6d9583302f32d2fe5500b524cbc59b3a18
MarceSanto <rocha.mer21@gmail.com>
2020-09-14 13:02:38 -0300

    começando o component chatWindow

---
+++ src/components/ChatWindow/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './index.css'
+
+export default () => {
+  return (
+    <div className='chatWindow'>
+      <div className='chatWindow--header'></div>
+      <div className='chatWindow--body'></div>
+      <div className='chatWindow--footer'></div>
+    </div>
+  )
+}
